0b601501637c4ed1b9fb105e6e37748877517468
Chandra-Mauli-Sharma <chandramaulisharma26@gmail.com>
2021-09-09 20:52:05 +0530

    Stress Started

---
+++ Assets/Spin.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject planet ;
+    public GameObject car;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            planet.transform.Rotate(0.0f, -10.0f*Time.deltaTime, 0.0f*Time.deltaTime, Space.Self);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            planet.transform.Rotate(5.0f*Time.deltaTime, 0.0f*Time.deltaTime, 0.0f*Time.deltaTime, Space.Self);
+            car.transform.Rotate(0.0f*Time.deltaTime, -10.0f*Time.deltaTime, 0.0f*Time.deltaTime, Space.Self);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            planet.transform.Rotate(-5.0f*Time.deltaTime, 0.0f*Time.deltaTime, 0.0f*Time.deltaTime, Space.Self);
+            car.transform.Rotate(0.0f*Time.deltaTime, 10.0f*Time.deltaTime, 0.0f*Time.deltaTime, Space.Self);
+        }
+
+    }
+}
